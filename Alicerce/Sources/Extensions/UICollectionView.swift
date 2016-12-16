ed39ca93dac16d337cf165db5e90cdcfef278bb0
Luís Portela Afonso <luis.afonso@mindera.com>
2016-12-16 02:29:02 +0000

    UIKit - UICollectionView

---
+++ Alicerce/Sources/Extensions/UICollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  UICollectionView.swift
+//  Alicerce
+//
+//  Created by Luís Afonso on 16/12/2016.
+//  Copyright © 2016 Mindera. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionView {
+    
+    func cell<T: ViewCellProtocol>(`for` indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            assertionFailure("🔥 Did you forgot to register cell with identifier `\(T.reuseIdentifier)` for type: `\(T.self)`")
+            return T()
+        }
+        
+        return cell
+    }
+}
