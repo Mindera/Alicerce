b00be7b2f13f23200ef515f195b6fd19c236450d
André Pacheco Neves <p4checo@users.noreply.github.com>
2017-04-18 11:47:25 +0100

    Add `TODO` and `FIXME` helper placeholders (#27)
    These placeholders help during development by:
    - Generating warnings for unimplemented or unfinished code
    - Crashing on unimplemented code (`TODO`’s)
    - Doing nothing on unfinished code (`FIXME`)
    
    Credits to @UINT_MIN and @shaps 🙏
    https://twitter.com/UINT_MIN/status/836316697388695552
    https://twitter.com/shaps/status/836353195098189824

---
+++ Alicerce/Sources/Utils/Placeholder.swift
@@ -0,0 +1,19 @@
+//
+//  Placeholder.swift
+//  Alicerce
+//
+//  Created by André Pacheco Neves on 15/04/2017.
+//  Copyright © 2017 Mindera. All rights reserved.
+//
+
+import Foundation
+
+// Credits to @UINT_MIN, @shaps 🙏
+// https://twitter.com/UINT_MIN/status/836316697388695552
+// https://twitter.com/shaps/status/836353195098189824
+
+@available(*, deprecated, message: "Implement this!")
+var TODO: Never { fatalError("💥: unimplemented!") }
+
+@available(*, deprecated, message: "Fix this code!")
+var FIXME: Void { return }
