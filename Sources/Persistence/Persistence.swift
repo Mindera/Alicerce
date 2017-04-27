11cd1010187cde272f2e9eadf5e212bd263ba793
André Pacheco Neves <p4checo@users.noreply.github.com>
2017-04-27 15:56:38 +0100

    Rename `Persistence` to `PersistenceStack` (#48)
    For consistency’s sake, renamed `Persistence` protocol to
    `PersistenceStack`, and created a caseless `enum` `Persistence` to
    contain persistence specific types such as `Key` and `Error`.
    
    Moved all CoreData persistence stack code into a separate CoreData
    folder.

--- Sources/Persistence/Persistence.swift
+++ Sources/Persistence/Persistence.swift
@@ -2,25 +2,19 @@
 //  Persistence.swift
 //  Alicerce
 //
-//  Created by Luís Portela on 13/04/2017.
+//  Created by André Pacheco Neves on 27/04/2017.
 //  Copyright © 2017 Mindera. All rights reserved.
 //
 
 import Foundation
 
-public enum PersistenceError: Swift.Error {
-    case noObjectForKey
-    case other(Swift.Error)
+public enum Persistence {
+
+    public typealias Key = String
+
+    public enum Error: Swift.Error {
+        case noObjectForKey
+        case other(Swift.Error)
+    }
 }
 
-public protocol Persistence {
-    typealias Key = String
-
-    typealias CompletionClosure<R> = (_ inner: () throws -> R) -> Void
-
-    func object(`for` key: Key, completion: @escaping CompletionClosure<Data>)
-
-    func setObject(_ object: Data, for key: Key, completion: @escaping CompletionClosure<Void>)
-
-    func removeObject(for key: String, completion: @escaping CompletionClosure<Void>)
-}
---
+++ Sources/Persistence/PersistenceStack.swift
@@ -0,0 +1,20 @@
+//
+//  PersistenceStack.swift
+//  Alicerce
+//
+//  Created by Luís Portela on 13/04/2017.
+//  Copyright © 2017 Mindera. All rights reserved.
+//
+
+import Foundation
+
+public protocol PersistenceStack {
+
+    typealias CompletionClosure<R> = (_ inner: () throws -> R) -> Void
+
+    func object(`for` key: Persistence.Key, completion: @escaping CompletionClosure<Data>)
+
+    func setObject(_ object: Data, for key: Persistence.Key, completion: @escaping CompletionClosure<Void>)
+
+    func removeObject(for key: String, completion: @escaping CompletionClosure<Void>)
+}
