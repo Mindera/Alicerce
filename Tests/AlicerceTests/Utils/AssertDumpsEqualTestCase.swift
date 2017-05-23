66b2509c722b0165e54ce1084bc987d8d8f6f9e8
André Pacheco Neves <p4checo@users.noreply.github.com>
2017-05-23 11:14:19 +0100

    Add `assertDumpsEqual` helper function (#59)
    Added `assertDumpsEqual` function to help validate `Equatable`
    conformances, as well as signal outdated implementations automatically
    on debug builds.
    
    Credits to Ole Begemann (@olebegemann) and Tim Vermeulen
    (@tim_vermeulen) 🙏
    https://oleb.net/blog/2017/03/dump-as-equatable-safeguard/

---
+++ Tests/AlicerceTests/Utils/AssertDumpsEqualTestCase.swift
@@ -0,0 +1,64 @@
+//
+//  AssertDumpsEqualTestCase.swift
+//  Alicerce
+//
+//  Created by André Pacheco Neves on 22/05/2017.
+//  Copyright © 2017 Mindera. All rights reserved.
+//
+
+import XCTest
+@testable import Alicerce
+
+class AssertDumpsEqualTestCase: XCTestCase {
+
+    func test_WithEqualString_ShouldSucceed() {
+        let s1 = "test"
+        let s2 = "test"
+
+        assertDumpsEqual(s1, s2)
+    }
+
+    func test_WithEqualInt_ShouldSucceed() {
+        let i1 = 1337
+        let i2 = 1337
+
+        assertDumpsEqual(i1, i2)
+    }
+
+    func test_WithEqualDate_ShouldSucceed() {
+        let d = Date()
+        let d1 = d
+        let d2 = d
+
+        assertDumpsEqual(d1, d2)
+    }
+
+    func test_WithEqualRange_ShouldSucceed() {
+        let r1 = 0..<1337
+        let r2 = 0..<1337
+
+        assertDumpsEqual(r1, r2)
+    }
+
+    func test_WithEqualStringArray_ShouldSucceed() {
+        let a1 = ["a", "b", "c"]
+        let a2 = ["a", "b", "c"]
+
+        assertDumpsEqual(a1, a2)
+    }
+
+    func test_WithEqualIntArray_ShouldSucceed() {
+        let a1 = [1, 2, 3]
+        let a2 = [1, 2, 3]
+
+        assertDumpsEqual(a1, a2)
+    }
+
+    func test_WithEqualDict_ShouldSucceed() {
+        let d1 = ["a" : 1, "b" : 2, "c" : 3]
+        let d2 = ["a" : 1, "b" : 2, "c" : 3]
+
+        assertDumpsEqual(d1, d2)
+    }
+    
+}
