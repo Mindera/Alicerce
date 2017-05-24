ee409d2e92ba72f367bff0df1343274a030b82e4
Luís Portela Afonso <luis.portela@mindera.com>
2017-05-24 19:27:02 +0100

    Fix method access level on CollectionViewCell

--- Sources/UIKit/CollectionViewCell.swift
+++ Sources/UIKit/CollectionViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 open class CollectionViewCell: UICollectionViewCell {
 
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
 
         setUpSubviews()
