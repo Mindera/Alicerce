f83dc66f0a161a770dd41bcc798c68412ebf221b
Luís Portela Afonso <me+github@portellaa.eu>
2017-06-13 10:04:13 +0100

    `Public` strikes again 😆 (#69)
    Added missing public access control on init

--- Sources/UIKit/CollectionReusableView.swift
+++ Sources/UIKit/CollectionReusableView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 open class CollectionReusableView: UICollectionReusableView {
 
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
 
         setUpSubviews()
