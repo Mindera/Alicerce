cdcbdb2b78ef209d786deca21420e46c33b6fce5
Luís Portela Afonso <luis.portela@mindera.com>
2017-12-15 17:04:08 +0000

    Performance Metrics Layer ✨

---
+++ Sources/PerformanceMetrics/PerformanceMetricsTracker.swift
@@ -0,0 +1,14 @@
+//
+//  PerformanceMetricsTracker.swift
+//  Alicerce
+//
+//  Created by Luís Portela on 15/12/2017.
+//  Copyright © 2017 Mindera. All rights reserved.
+//
+
+import Foundation
+
+public protocol PerformanceMetricsTracker: class {
+    func begin(with identifier: PerformanceMetrics.Identifier)
+    func end(with identifier: PerformanceMetrics.Identifier)
+}
