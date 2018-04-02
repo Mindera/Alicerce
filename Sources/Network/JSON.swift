f1d5d311c3f0f5017ca96ba1d9c59205d2d1f7a7
Luís Portela Afonso <luis.portela@mindera.com>
2018-04-02 23:08:04 +0100

    JSON Mappable - Mappable all the things

--- Sources/Network/JSON.swift
+++ Sources/Network/JSON.swift
@@ -186,6 +186,61 @@ public enum JSON {
         return value
     }
 
+    /// Parse an attribute of type `T: Mappable` with the given key on the given JSON dictionary, validating if
+    /// the value can be used to instantiate a Mappable model. Additionally, an optional `parseAPIError` closure can be supplied
+    /// so that if a parsing step fails an attempt is made to extract a domain specific error and throw it.
+    ///
+    /// - Parameters:
+    ///   - type: The type of the attribute to parse.
+    ///   - key: The JSON attribute key to parse.
+    ///   - json: The JSON dictionary.
+    ///   - parseAPIError: The API error parsing closure.
+    /// - Returns: The value of type `T` associated to the given attribute's key.
+    /// - Throws: An error of type `JSON.Error`, or a domain specific `Swift.Error` produced by `parseAPIError`.
+    public static func parseMappableAttribute<T: Mappable>(
+        _ type: T.Type,
+        key: JSON.AttributeKey,
+        json: JSON.Dictionary,
+        parseAPIError: ParseAPIErrorClosure? = nil) throws -> T {
+
+        guard let anyValue = json[key] else {
+            throw parseAPIError?(json) ?? Error.missingAttribute(key, json: json)
+        }
+
+        let anyObject: Any = try parseValue(rawValue: anyValue, key: key, json: json, where: nil, parseAPIError: parseAPIError)
+
+        return try T.model(from: anyObject)
+    }
+
+    /// Parse an ++optional++ attribute of type `T: Mappable` with the given key on the given JSON dictionary,
+    /// validating if the raw value can be used to instantiate it. Additionally, an optional `parseAPIError` closure
+    /// can be supplied so that if a parsing step fails an attempt is made to extract a domain specific error and throw
+    /// it. If the key doesn't exist on the JSON, `nil` is returned unless an API error is parsed (and thrown),
+    /// otherwise standard validations are made.
+    ///
+    /// - Parameters:
+    ///   - type: The type of the attribute to parse.
+    ///   - key: The JSON attribute key to parse.
+    ///   - json: The JSON dictionary.
+    ///   - parseAPIError: The API error parsing closure.
+    /// - Returns: The value of type `T` associated to the given attribute's key.
+    /// - Throws: An error of type `JSON.Error`, or a domain specific `Swift.Error` produced by `parseAPIError`.
+    public static func parseOptionalMappableAttribute<T: Mappable>(
+        _ type: T.Type,
+        key: JSON.AttributeKey,
+        json: JSON.Dictionary,
+        parseAPIError: ParseAPIErrorClosure? = nil) throws -> T? {
+
+        guard let anyValue = json[key] else {
+            if let apiError = parseAPIError?(json) { throw apiError }
+            return nil
+        }
+
+        let anyObject: Any = try parseValue(rawValue: anyValue, key: key, json: json, where: nil, parseAPIError: parseAPIError)
+
+        return try T.model(from: anyObject)
+    }
+
     // MARK: - Inferred Type
 
     /// Parse an attribute of type `T` with the given key on the given JSON dictionary, and optionally validate
@@ -326,6 +381,44 @@ public enum JSON {
         }
 
         return date
+    }
+
+    /// Parse an attribute of type `T: Mappable` with the given key on the given JSON dictionary, validating if
+    /// the raw value can be used to instantiate it. Additionally, an optional `parseAPIError` closure can be supplied
+    /// so that if a parsing step fails an attempt is made to extract a domain specific error and throw it.
+    ///
+    /// - Parameters:
+    ///   - type: The type of the attribute to parse.
+    ///   - key: The JSON attribute key to parse.
+    ///   - json: The JSON dictionary.
+    ///   - parseAPIError: The API error parsing closure.
+    /// - Returns: The value of type `T` associated to the given attribute's key.
+    /// - Throws: An error of type `JSON.Error`, or a domain specific `Swift.Error` produced by `parseAPIError`.
+    public static func parseMappableAttribute<T: Mappable>(
+        _ key: JSON.AttributeKey,
+        json: JSON.Dictionary,
+        parseAPIError: ParseAPIErrorClosure? = nil) throws -> T {
+        return try parseMappableAttribute(T.self, key: key, json: json, parseAPIError: parseAPIError)
+    }
+
+    /// Parse an ++optional++ attribute of type `T: Mappable` with the given key on the given JSON dictionary,
+    /// validating if the raw value can be used to instantiate it. Additionally, an optional `parseAPIError` closure
+    /// can be supplied so that if a parsing step fails an attempt is made to extract a domain specific error and throw
+    /// it. If the key doesn't exist on the JSON, `nil` is returned unless an API error is parsed (and thrown),
+    /// otherwise standard validations are made.
+    ///
+    /// - Parameters:
+    ///   - type: The type of the attribute to parse.
+    ///   - key: The JSON attribute key to parse.
+    ///   - json: The JSON dictionary.
+    ///   - parseAPIError: The API error parsing closure.
+    /// - Returns: The value of type `T` associated to the given attribute's key.
+    /// - Throws: An error of type `JSON.Error`, or a domain specific `Swift.Error` produced by `parseAPIError`.
+    public static func parseOptionalMappableAttribute<T: Mappable>(
+        _ key: JSON.AttributeKey,
+        json: JSON.Dictionary,
+        parseAPIError: ParseAPIErrorClosure? = nil) throws -> T? {
+        return try parseOptionalMappableAttribute(T.self, key: key, json: json, parseAPIError: parseAPIError)
     }
 
     // MARK: - Private methods
