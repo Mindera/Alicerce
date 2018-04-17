60e7fba135e140a540c883048b2a33f8171314a3
André Pacheco Neves <andre.neves@mindera.com>
2018-04-17 11:31:55 +0100

    Add `Atomic` ☢️ and `Lock` 🔒 (#114)
    - Copied and modified `Atomic` and `Lock` from `ReactiveSwift` 🙏
    
    - Converted the existing `Atomic` UT's and added some on `Lock`.
    
    - Cleaned up test target project settings.

---
+++ Tests/AlicerceTests/Utils/AtomicTestCase.swift
@@ -0,0 +1,83 @@
+//
+//  AtomicTestCase.swift
+//  AlicerceTests
+//
+//  Created by André Pacheco Neves on 13/04/2018.
+//  Copyright © 2018 Mindera. All rights reserved.
+//
+
+import XCTest
+@testable import Alicerce
+
+class AtomicTestCase: XCTestCase {
+
+    private var atomic: Atomic<Int>!
+    
+    override func setUp() {
+        super.setUp()
+
+        atomic = Atomic(0)
+    }
+    
+    override func tearDown() {
+        atomic = nil
+
+        super.tearDown()
+    }
+
+    func testRead_ShouldReturnValue() {
+        XCTAssertEqual(atomic.value, 0)
+    }
+
+    func testWrite_ShouldUpdateValue() {
+        atomic.value = 1337
+        XCTAssertEqual(atomic.value, 1337)
+    }
+
+    func testSwap_ShouldSwapValue() {
+        XCTAssertEqual(atomic.swap(1337), 0)
+        XCTAssertEqual(atomic.value, 1337)
+    }
+
+    func testModify_ShouldModifyValue() {
+        atomic.modify { $0 += 1337 }
+        XCTAssertEqual(atomic.value, 1337)
+    }
+
+    func testModify_WithConcurrentAccess_ShouldEnsureThreadSafetyOfCriticalSection() {
+        let numWrites = 1000
+        let numQueues = 4
+
+        let writeExpectation = expectation(description: "modify")
+        writeExpectation.expectedFulfillmentCount = numWrites * numQueues
+
+        let queues: [OperationQueue] = (0..<numQueues).map { _ in
+            let q = OperationQueue()
+            q.isSuspended = true
+            return q
+        }
+
+        for _ in 1...numWrites {
+            queues.forEach {
+                $0.addOperation {
+                    self.atomic.modify { $0 += 1 }
+                    writeExpectation.fulfill()
+                }
+            }
+        }
+
+        // wait for all work to be scheduled before starting writes
+        queues.forEach { $0.isSuspended = false }
+
+        waitForExpectations(timeout: 1)
+
+        XCTAssertEqual(atomic.value, numWrites * numQueues)
+    }
+
+    func testWithValue_ShouldPerformAnActionWithTheValue() {
+        let result: Bool = atomic.withValue { $0 == 0 }
+
+        XCTAssertTrue(result)
+        XCTAssertEqual(atomic.value, 0)
+    }
+}
---
+++ Tests/AlicerceTests/Utils/UnfairLockTestCase.swift
@@ -0,0 +1,144 @@
+//
+//  UnfairLockTestCase.swift
+//  AlicerceTests
+//
+//  Created by André Pacheco Neves on 16/04/2018.
+//  Copyright © 2018 Mindera. All rights reserved.
+//
+
+import XCTest
+@testable import Alicerce
+
+class UnfairLockTestCase: XCTestCase {
+
+    private var lock: Lock.UnfairLock!
+    private var queue: DispatchQueue!
+
+    override func setUp() {
+        super.setUp()
+
+        lock = Lock.UnfairLock()
+        queue = DispatchQueue(label: "testQueue", qos: .default)
+    }
+
+    override func tearDown() {
+        lock = nil
+        queue = nil
+
+        super.tearDown()
+    }
+
+    func testLock_ShouldLock() {
+        let lockExpectation = expectation(description: "lock")
+
+        queue.asyncAfter(deadline: .now() + .milliseconds(10)) {
+            XCTAssertFalse(self.lock.try())
+            lockExpectation.fulfill()
+        }
+
+        lock.lock()
+
+        waitForExpectations(timeout: 1)
+
+        lock.unlock()
+    }
+
+    func testUnlock_ShouldUnlock() {
+        let tryExpectation = expectation(description: "try")
+        let lockExpectation = expectation(description: "lock")
+        let unlockExpectation = expectation(description: "unlock")
+
+        let sem = DispatchSemaphore(value: 0)
+
+        queue.asyncAfter(deadline: .now() + .milliseconds(10)) {
+            XCTAssertFalse(self.lock.try())
+            tryExpectation.fulfill()
+
+            self.lock.lock() // should block until unlocked
+            lockExpectation.fulfill()
+
+            sem.wait()
+
+            self.lock.unlock()
+            unlockExpectation.fulfill()
+        }
+
+        lock.lock()
+
+        wait(for: [tryExpectation], timeout: 1)
+
+        lock.unlock()
+
+        wait(for: [lockExpectation], timeout: 1)
+
+        XCTAssertFalse(self.lock.try())
+        sem.signal()
+
+        wait(for: [unlockExpectation], timeout: 1)
+    }
+
+    func testTry_WithLockedLock_ShouldReturnFalse() {
+        let tryLockExpectation = expectation(description: "try")
+
+        queue.asyncAfter(deadline: .now() + .milliseconds(10)) {
+            XCTAssertFalse(self.lock.try())
+            tryLockExpectation.fulfill()
+        }
+
+        lock.lock()
+
+        waitForExpectations(timeout: 1)
+
+        lock.unlock()
+    }
+
+    func testTry_WithUnlockedLock_ShouldReturnTrueAndLock() {
+        let tryLockExpectation = expectation(description: "try")
+
+        queue.asyncAfter(deadline: .now() + .milliseconds(10)) {
+            XCTAssertFalse(self.lock.try())
+            tryLockExpectation.fulfill()
+        }
+
+        XCTAssertTrue(self.lock.try())
+
+        waitForExpectations(timeout: 1)
+
+        lock.unlock()
+    }
+
+    func testLockUnlock_WithConcurrentAccess_ShouldEnsureThreadSafetyOfCriticalSection() {
+        let numWrites = 1000
+        let numQueues = 4
+
+        let writeExpectation = expectation(description: "write")
+        writeExpectation.expectedFulfillmentCount = numWrites * numQueues
+
+        let queues: [OperationQueue] = (0..<numQueues).map { _ in
+            let q = OperationQueue()
+            q.isSuspended = true
+            return q
+        }
+
+        let box = VarBox(0)
+
+        for _ in 1...numWrites {
+            queues.forEach {
+                $0.addOperation {
+                    self.lock.lock()
+                    box.value += 1
+                    self.lock.unlock()
+                    writeExpectation.fulfill()
+                }
+            }
+        }
+
+        // wait for all work to be scheduled before starting writes
+        queues.forEach { $0.isSuspended = false }
+
+        waitForExpectations(timeout: 1)
+
+        XCTAssertEqual(box.value, numWrites * numQueues)
+    }
+}
+
