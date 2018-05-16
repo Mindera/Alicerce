d86e8edae50dc2ceec7046dfbb98a5499ecb1cfd
Filipe Lemos <filipe.lemos@mindera.com>
2018-05-16 13:01:04 +0100

    Rename `BuildableCache`
    * Rename `GeneratableObjectCache` to `BuildableCache`
    * Rename `value` method to `object` to more closely follow cache naming conventions
    * Add `evict` and `evictAll` methods to cache
    * Add unit tests for new evict methods

---
+++ Sources/Utils/BuildableCache.swift
@@ -0,0 +1,39 @@
+//
+//  BuildableCache.swift
+//  Alicerce
+//
+//  Created by Filipe Lemos on 02/05/2018.
+//  Copyright © 2018 Mindera. All rights reserved.
+//
+
+import Foundation
+
+public protocol BuildableKey: Hashable {
+    associatedtype T
+
+    func build() -> T
+}
+
+public final class BuildableCache<T> {
+
+    private let cache: Atomic<[AnyHashable : T]> = Atomic([:])
+
+    public func object<Key: BuildableKey>(_ key: Key) -> T where Key.T == T {
+
+        return cache.modify {
+            if let cached = $0[key] { return cached }
+
+            let built = key.build()
+            $0[key] = built
+            return built
+        }
+    }
+
+    public func evict<Key: BuildableKey>(_ key: Key) {
+        cache.modify { $0.removeValue(forKey: key) }
+    }
+
+    public func evictAll() {
+        cache.modify { $0.removeAll() }
+    }
+}
