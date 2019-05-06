ec2b9f63a793e36bab95f04afabfe1a5230ff4a3
André Pacheco Neves <andre.neves@mindera.com>
2019-05-06 10:19:20 +0100

    Revert Atomic changes

--- Sources/Utils/Atomic.swift
+++ Sources/Utils/Atomic.swift
@@ -32,12 +32,6 @@ public final class Atomic<Value> {
     public var value: Value {
         get { return withValue { $0 } }
         set(newValue) { swap(newValue) }
-        _modify {
-            lock.lock()
-            defer { lock.unlock() }
-
-            yield &_value
-        }
     }
 
     /// Initialize the variable with the given initial value.
