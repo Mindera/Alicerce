9c6a430db430ed5d89a086f70810949c40e7d611
Mickael Domingues <mickael.domingues@mindera.com>
2020-01-20 14:48:42 +0000

    Remove then function
    Code review changes.

--- Tests/AlicerceTests/Extensions/Foundation/OptionalTests.swift
+++ Tests/AlicerceTests/Extensions/Foundation/OptionalTests.swift
@@ -4,25 +4,6 @@ import XCTest
 
 final class OptionalTests: XCTestCase {
 
-    func testThen_UsingNonOptionalValue_ShouldUnwrapTheValue() {
-        var anOptionalString: String? = "😎"
-
-        if let value = anOptionalString {
-            XCTAssertEqual(anOptionalString, value, "🔥: \(value) not unwrapped! 😱")
-            anOptionalString = "😇"
-        }
-
-        XCTAssertEqual(anOptionalString, "😇", "🔥: then closure not executed! 😱")
-    }
-
-    func testThen_UsingOptionalValue_ShoudNotUnwrapTheValue() {
-        let nullValue: String? = nil
-
-        if let _ = nullValue {
-            XCTFail("💥 nil unwrapped 😱")
-        }
-    }
-
     func testRequire_UsingNonOptionalValue_ShouldUnwrapTheValue() {
         let anOptionalString: String? = "😎"
 
