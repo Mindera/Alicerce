f8e6f53d97f11910f2f157450525a3ea50ecd1a8
André Pacheco Neves <andre.neves@mindera.com>
2020-10-02 14:58:13 +0100

    Add `inout` variant of `groupedReduce` (#221)
    Similar to the stdlib's `reduce`, the `groupedReduce` can provide an
    `inout` variant for improved efficiency when the group type has
    copy-on-write semantics (like `Array` or `Dictionary`).
    
    ## Changes
    
    - Create new `groupedReduce(into:combine:groupBy)` variant with an
    `inout` combine closure.

--- Sources/Extensions/Foundation/Sequence.swift
+++ Sources/Extensions/Foundation/Sequence.swift
@@ -14,14 +14,47 @@ public extension Sequence {
     ///   - combine: a closure that combines the accumulating value of a group and produces a new accumulating value.
     ///   - groupBy: a closure that produces a key for each element in the sequence.
     /// - Returns: a dictionary containing the final accumulated values for each produced key.
-    func groupedReduce<K: Hashable, U>(initial: U,
-                                       combine: (U, Iterator.Element) throws -> U,
-                                       groupBy: (Iterator.Element) throws -> K) rethrows -> [K : U] {
+    func groupedReduce<K: Hashable, U>(
+        initial: U,
+        combine: (U, Iterator.Element) throws -> U,
+        groupBy: (Iterator.Element) throws -> K
+    ) rethrows -> [K : U] {
+
         var result: [K : U] = [:]
 
         for element in self {
             let key = try groupBy(element)
             result[key] = try combine(result[key] ?? initial, element)
+        }
+
+        return result
+    }
+
+    /// Returns the result of combining the elements of the sequence using the given combining closure, grouped by keys
+    /// generated using the a grouping closure. The result is a dictionary of type `[K : U]`. An initial value should
+    /// be given to be used as initial accumulating value in each group.
+    ///
+    /// This method is preferred over `groupedReduce(initial:combine:groupBy:)` for efficiency when the group `U` is a
+    /// copy-on-write type, for example an Array or a Dictionary.
+    ///
+    /// - Parameters:
+    ///   - initial: a value to be used as the initial accumulating value in each group.
+    ///   - combine: a closure that combines the accumulating value of a group and produces a new accumulating value.
+    ///   - groupBy: a closure that produces a key for each element in the sequence.
+    /// - Returns: a dictionary containing the final accumulated values for each produced key.
+    func groupedReduce<K: Hashable, U>(
+        into initial: U,
+        combine: (inout U, Iterator.Element) throws -> Void,
+        groupBy: (Iterator.Element) throws -> K
+    ) rethrows -> [K : U] {
+
+        var result: [K : U] = [:]
+
+        for element in self {
+            let key = try groupBy(element)
+            var group = result[key] ?? initial
+            try combine(&group, element)
+            result[key] = group
         }
 
         return result
