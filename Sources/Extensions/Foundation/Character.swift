e2f74873ae3404b020729174c1f6986911e15dea
André Pacheco Neves <andre.neves@mindera.com>
2021-04-04 01:29:32 +0100

    Add `String` multi-replace via `Scanner` ⏩
    Sometimes we need to replace multiple characters in a given `String`,
    but both `replacingOccurrences()` and `replacingCharacters` operate on
    a single `String`/`Character`, requiring multiple passes and thus
    becoming very inefficient. One such example is to replace all line
    breaking characters in a string into their non line breaking version,
    which requires 6 substitution "passes" (space, hyphen, em dash, en
    dash, question mark and closing brace).
    
    By using a `Scanner` as a matching mechanism, we can implement
    multi-replace in a single pass on the string, greatly improving
    efficiency.
    
    ## Changes
    
    - Add new
    `String.replacingOccurrencesOfCharacters(in:skippingCharactersIn:)`
    extension to allow replacing multiple characters in a string in a
    single pass.
    
    - Add new `String.nonLineBreaking()` extension to convert a string into
    a non line breaking version.

---
+++ Sources/Extensions/Foundation/Character.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Character {
+
+    static let lineSeparator: Character = "\u{2028}"
+    static let nonBreakingSpace: Character = "\u{00a0}"
+    static let nonBreakingHyphen: Character = "\u{2011}"
+    static let wordJoiner: Character = "\u{2060}"
+    static let emDash: Character = "\u{2013}" // —
+    static let enDash: Character = "\u{2014}" // –
+}
--- Sources/Extensions/Foundation/String.swift
+++ Sources/Extensions/Foundation/String.swift
@@ -59,3 +59,91 @@ public extension String {
         dump(x, to: &self)
     }
 }
+
+extension String {
+
+    /// Replaces occurrences of multiple `Character`s with corresponding `String` values using the given mapping, while
+    /// skipping (filtering out) an optional set of characters from the output. Being backed by a `Scanner`, a single
+    /// pass is made over the receiver.
+    ///
+    /// - Parameters:
+    ///   - replacementMap: A dictionary containing the replacement mapping `Character` -> `String`.
+    ///   - charactersToBeSkipped: An optional set of characters to skip (i.e. filter out from the input).
+    /// - Returns: A modified version of the receiver with the replacement mapping applied.
+    public func replacingOccurrencesOfCharacters(
+        in replacementMap: [Character: String],
+        skippingCharactersIn charactersToBeSkipped: CharacterSet? = nil
+    ) -> String {
+
+        guard !replacementMap.isEmpty else { return self }
+
+        let matchSet = CharacterSet(charactersIn: replacementMap.keys.reduce(into: "") { $0 += String($1) })
+            .union(charactersToBeSkipped ?? CharacterSet())
+
+        var final = ""
+
+        let scanner = Scanner(string: self)
+        scanner.charactersToBeSkipped = charactersToBeSkipped
+
+        while !scanner.isAtEnd {
+
+            // copy everything until finding a character to be replaced or skipped
+            var collector: NSString? = ""
+            if scanner.scanUpToCharacters(from: matchSet, into: &collector), let collector = collector {
+                final.append(collector as String)
+            }
+
+            // exit early if we're already at the end
+            guard !scanner.isAtEnd else { break }
+
+            // skip characters if needed
+            if let charactersToBeSkipped = charactersToBeSkipped {
+                scanner.scanCharacters(from: charactersToBeSkipped, into: nil)
+            }
+
+            // find and replace matching character if needed
+            replacementMap
+                .first { match, _ in scanner.scanString(String(match), into: nil) }
+                .flatMap { _, replacement in final.append(replacement) }
+        }
+
+        return final
+    }
+}
+
+extension String {
+
+    public static let nonBreakingSpace = String(Character.nonBreakingSpace)
+    public static let nonBreakingHyphen = String(Character.nonBreakingHyphen)
+    public static let wordJoiner = String(Character.wordJoiner)
+    public static let emDash = String(Character.emDash)
+    public static let enDash = String(Character.enDash)
+
+    /// Returns a non line breaking version of `self`. Line breaking characters occurrences are replaced with
+    /// corresponding non line breaking variants when existent. Otherwise, word joiner characters are attached to them
+    /// to make them non line breaking.
+    ///
+    /// The character mapping is:
+    ///  - space (" ") -> non breaking space (`U+2028`)
+    ///  - hyphen ("-") -> non breaking hyphen (`U+00A0`)
+    ///  - em dash ("—") -> word joiner (`U+2060`) + em dash + word joiner (`U+2060`)
+    ///  - em dash ("–") -> word joiner (`U+2060`) + en dash + word joiner (`U+2060`)
+    ///  - question mark ("?") -> question mark + word joiner (`U+2060`)
+    ///  - closing brace ("}") -> closing brace + word joiner (`U+2060`)
+    ///
+    /// - Returns: A modified version of the receiver without line breaking characters.
+    public func nonLineBreaking() -> String {
+
+        replacingOccurrencesOfCharacters(
+            in: [
+                " ": String.nonBreakingSpace,
+                "-": String.nonBreakingHyphen,
+                .emDash: String([.wordJoiner, .emDash, .wordJoiner]),
+                .enDash: String([.wordJoiner, .enDash, .wordJoiner]),
+                "?": "?" + .wordJoiner,
+                "}": "}" + .wordJoiner,
+            ],
+            skippingCharactersIn: nil
+        )
+    }
+}
