932e082f4b194732f037350fce2586de9a7fc435
André Pacheco Neves <andre.neves@mindera.com>
2021-04-06 17:53:33 +0100

    Simplify `Analytics.MultiTracker` 👁 (#229)
    While our `Analytics.MultiTracker` was implemented to be dynamically
    configured by allowing registering/unregistering sub trackers, in
    reality this didn't bring much benefit (or at all) as from our
    experience the tracker is typically configured once during app
    startup. To allow this dynamism while being thread safe, it resorted to
    synchronization primitives (i.e. `Atomic`) on the state, which in
    practice causes one lock/unlock per track call (trackers). This
    is obviously a very high price to pay when we consider that the
    dynamism it allows is rarely used. So, this dynamism has been removed
    in favor of configuring the tracker once on `init`, passing in the
    sub-trackers to use.
    
    Additionally, some some minor improvements were made to Analytics
    related types.
    
    ## Changes
    
    - Make `Analytics.MultiTracker` have its sub-trackers be configured on
    `init`, avoiding complexity and contention due to handling concurrent
    operations.
    
    - Remove `ID` from `AnalyticsTracker`, as it was used mostly to keep
    track of registered trackers.
    
    - Add `eraseToAnyAnalyticsRracker()` helper in `AnalyticsTracker` to
    facilitate type erasing trackers to passinto `Analytics.MultiTracker`s.
    
    - Add `_wrapped` helper property in `AnyAnalyticsTracker` to hold
    a reference to the wrapped tracker instance.

--- Sources/Analytics/Trackers/Analytics+MultiTracker.swift
+++ Sources/Analytics/Trackers/Analytics+MultiTracker.swift
@@ -2,64 +2,20 @@ import Foundation
 
 public extension Analytics {
 
-    /// An error produced by `MultiTracker` instances.
-    enum MultiTrackerError: Swift.Error {
-
-        /// A tracker with the same id already registered.
-        case duplicateTracker(AnalyticsTracker.ID)
-
-        /// A tracker with the given id isn't registered.
-        case inexistentTracker(AnalyticsTracker.ID)
-    }
-
     /// An analytics tracker that forwards analytics events to multiple trackers, while not doing any tracking on its
     /// own.
     final class MultiTracker<State, Action, ParameterKey: AnalyticsParameterKey>: AnalyticsTracker {
 
-        /// The registered sub trackers (read only).
-        public var trackers: [AnyAnalyticsTracker<State, Action, ParameterKey>] { return _trackers.value }
-
         /// The registered sub trackers.
-        private let _trackers: Atomic<[AnyAnalyticsTracker<State, Action, ParameterKey>]>
+        public let trackers: [AnyAnalyticsTracker<State, Action, ParameterKey>]
 
         /// Creates an analytics multi tracker instance.
-        public init() {
-            self._trackers = Atomic<[AnyAnalyticsTracker<State, Action, ParameterKey>]>([])
-        }
+        /// - Parameter trackers: The analytics trackers to register.
+        public init(trackers: [AnyAnalyticsTracker<State, Action, ParameterKey>]) {
 
-        // MARK: - Sub-Tracker Management
+            assert(!trackers.isEmpty, "🙅‍♂️ Trackers shouldn't be empty, since it renders this tracker useless!")
 
-        /// Registers a sub tracker, and starts sending any new analytics events to it. This method is thread safe.
-        ///
-        /// - Parameter tracker: The analytics tracker to register.
-        /// - Throws: An `Analytics.MultiTrackerError.duplicateTracker` error if a tracker with the same `id` is
-        /// already registered.
-        public func register<T: AnalyticsTracker>(_ tracker: T) throws
-        where T.State == State, T.Action == Action, T.ParameterKey == ParameterKey {
-            precondition(tracker.id != id, "🙅‍♂️ Can't register a tracker with the same `id` as `self`!")
-
-            try _trackers.modify {
-                guard $0.contains(where: { $0.id == tracker.id }) == false else {
-                    throw MultiTrackerError.duplicateTracker(tracker.id)
-                }
-                $0.append(AnyAnalyticsTracker(tracker))
-            }
-        }
-
-        /// Unregisters a sub tracker, preventing any new analytics events from being sent to it. This method is thread
-        /// safe.
-        ///
-        /// - Parameter tracker: The analytics tracker to unregister.
-        /// - Throws: An `Analytics.MultiTrackerError.inexistentTracker` error if a tracker with the same `id` isn't
-        /// registered.
-        public func unregister<T: AnalyticsTracker>(_ tracker: T) throws
-        where T.State == State, T.Action == Action, T.ParameterKey == ParameterKey {
-            try _trackers.modify {
-                guard $0.contains(where: { $0.id == tracker.id }) else {
-                    throw MultiTrackerError.inexistentTracker(tracker.id)
-                }
-                $0 = $0.filter { $0.id != tracker.id }
-            }
+            self.trackers = trackers
         }
 
         // MARK: - Tracking
@@ -68,11 +24,8 @@ public extension Analytics {
         ///
         /// - Parameter event: The event to track.
         public func track(_ event: Analytics.Event<State, Action, ParameterKey>) {
-            let currentTrackers = _trackers.value
 
-            guard currentTrackers.isEmpty == false else { return }
-
-            currentTrackers.forEach { $0.track(event) }
+            trackers.forEach { $0.track(event) }
         }
     }
 }
--- Sources/Analytics/Trackers/AnyAnalyticsTracker.swift
+++ Sources/Analytics/Trackers/AnyAnalyticsTracker.swift
@@ -5,8 +5,8 @@ extension Analytics {
     /// A type-erased analytics tracker.
     public final class AnyAnalyticsTracker<State, Action, ParameterKey: AnalyticsParameterKey>: AnalyticsTracker {
 
-        /// The type-erased tracker's wrapped instance id.
-        public let id: ID
+        /// The type-erased tracker's wrapped instance.
+        public let _wrapped: AnyObject
 
         /// The type-erased tracker's wrapped instance `track` method, stored as a closure.
         private let _track: (Analytics.Event<State, Action, ParameterKey>) -> Void
@@ -17,15 +17,14 @@ extension Analytics {
         ///   - tracker: The analytics tracker instance to wrap.
         public init<T: AnalyticsTracker>(_ tracker: T)
         where T.State == State, T.Action == Action, T.ParameterKey == ParameterKey {
-            id = tracker.id
+
+            _wrapped = tracker
             _track = tracker.track
         }
 
         /// Tracks an analytics event via the wrapped tracker.
         ///
         /// - Parameter event: The analytics event.
-        public func track(_ event: Analytics.Event<State, Action, ParameterKey>) {
-            _track(event)
-        }
+        public func track(_ event: Analytics.Event<State, Action, ParameterKey>) { _track(event) }
     }
 }
