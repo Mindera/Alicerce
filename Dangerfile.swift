dbf9c95842d37b2b1008e712ecef1ee15947bacc
Eduardo Lacerda <eduardo.lacerda@mindera.com>
2021-08-03 10:32:56 -0300

    Remove List of Changed File from Danger rules

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -6,7 +6,3 @@ let danger = Danger()
 
 // Swiftlint style check, with comment on PR
 SwiftLint.lint(.all(directory: nil), inline: true)
-
-// List of all edited files
-let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined(separator: ", "))")
